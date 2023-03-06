f0efec577c6139a789ff7b7b240ee62ec212c303
garden <gkdlqkql6@naver.com>
2023-03-06 19:08:17 +0900

    start with springboot initializer

---
+++ src/main/java/com/example/gardencoffeeproduct/GardenCoffeeProductApplication.java
@@ -0,0 +1,13 @@
+package com.example.gardencoffeeproduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GardenCoffeeProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GardenCoffeeProductApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gardencoffeeproduct/GardenCoffeeProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gardencoffeeproduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GardenCoffeeProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
