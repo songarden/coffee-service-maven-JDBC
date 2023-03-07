30ba8a1a18b37074fc3628606e9bc83358dc3f72
Son Jung Won <gkdlqkql6@naver.com>
2023-03-07 17:41:51 +0900

    sql 스키마

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,10 @@
+create TABLE products
+(
+    product_id BINARY(16) PRIMARY KEY,
+    product_name VARCHAR(20) NOT NULL,
+    category VARCHAR(50) NOT NULL,
+    price bigint NOT NULL,
+    description VARCHAR(500) DEFAULT NULL,
+    created_at datetime(6) NOT NULL,
+    updated_at DATETIME(6) DEFAULT NULL
+)
